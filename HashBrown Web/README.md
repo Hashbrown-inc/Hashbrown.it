952f95ceb87972058bf1a28d8dbd147a2e3c01db
Nick <88886207+nickedchen@users.noreply.github.com>
2023-03-09 16:02:38 -0800

    starter code round 2

---
+++ HashBrown Web/README.md
@@ -0,0 +1,3 @@
+## Hashbrown Demo Page
+
+This is a demo page for the [Hashbrown]("https://github.com/ormaq/Hashbrown") library.
---
+++ HashBrown Web/src/App.js
@@ -0,0 +1,24 @@
+import "./raster.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="title">Introducing Hashbrown.</h1>
+      <r-grid columns="6">
+        <r-cell span="2">
+          <p>Hashbrown is a blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah.</p>
+        </r-cell>
+
+        <r-cell span="4">
+          <p>
+            Hashbrown uses blah blah blah to keep your files secure using the blah blah blah protocols. It allows blah
+            blah blah blah blah blah blah blah blah blah blah blah without blah blah blah blah blah blah blah blah blah
+            blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah
+          </p>
+        </r-cell>
+      </r-grid>
+    </div>
+  );
+}
+
+export default App;
