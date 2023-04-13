59c80992504f968ea5626094178014951c9f5885
ortap <jjelder@live.com>
2023-04-13 00:02:02 -0700

    Quickhack

--- src/pages/Playground.js
+++ src/pages/Playground.js
@@ -16,73 +16,10 @@ const Playground = () => {
 
 
   return (
-    <div className="playground">
-      <Helmet>
-        <style>{":root {--background-color: #F0B03D; --foreground-color: #000000;  font-size: 14px; }"}</style>
-      </Helmet>
-
-      <h1 className={style.playgroundTitle}>Playground</h1>
-      <div className="grid grid-cols-6 gap-12 order-1">
-        <div className="col-span-2">
-          <div className="pe-4">
-            <form action="/upload" method="POST" enctype="multipart/form-data" name="anyfile">
-              <h4 className="pb-2 tracking-tight">File Upload</h4>
-              <div className="flex items-start justify-center w-full font-mono">
-                <label
-                  for="dropzone-file"
-                  className="flex flex-col items-start justify-center w-full border-2 border-gray-300 border-none rounded-md cursor-pointer bg-white">
-                  <p className="text-sm font-semibold text-gray-100 p-4 tracking-tighter opacity-50">
-                    Upload some file here. Max 5MB in size.
-                  </p>
-                  <input id="dropzone-file" type="file" className="hidden" />
-                </label>
-              </div>
-              <button className=" block w-50 py-3 px-6 mt-4 text-white rounded-lg bg-gray-50 outline-0 sm:text-md font-semibold transition duration-300 ease-in-out bg-brown hover:scale-95">
-                {" "}
-                Whoosh !{" "}
-              </button>
-            </form>
-            <form action="/upload" method="POST" enctype="multipart/form-data" name="txtfile">
-              <h4 className="pb-2 pt-4 tracking-tight">Or type something down here</h4>
-              <div className="flex items-start justify-center w-full font-mono">
-                <input
-                  type="text"
-                  className="block w-full p-4 text-gray-900 rounded-lg outline-0 sm:text-sm font-semibold tracking-tighter bg-white "
-                  placeholder="Type some text here ..."
-                />
-              </div>
-              <button className="block w-50 py-3 px-6 mt-4 text-white rounded-lg bg-gray-50 outline-0 sm:text-md font-semibold transition duration-300 ease-in-out bg-brown hover:scale-95">
-                {" "}
-                Upload as .txt file{" "}
-              </button>
-
-              <h4 className="pb-2 pt-9 tracking-tight">File Hash</h4>
-              <div className="flex items-start justify-center w-full font-mono">
-                <p className="block w-full p-4 text-gray-900 rounded-lg outline-0 sm:text-sm font-semibold tracking-tight bg-white">
-                  {!data ? "Unknown"
-                  : data}
-                </p>
-              </div>
-            </form>
-
-            <h4 className="pb-2 pt-4 tracking-tight">Gas Price</h4>
-            <div className="flex items-start justify-center w-full font-mono">
-              <p className="block w-full p-4 text-gray-900 rounded-lg outline-0 sm:text-sm font-semibold tracking-tighter bg-white opacity-90">
-                0
-              </p>
-            </div>
-          </div>
-          </div>
-          <div className="col-span-4 order-2">
-              <h4 className="pb-2 tracking-tight">Code execution</h4>
-              <div className="flex items-start justify-center h-90 font-mono">
-                <p className="block w-full overflow-y-auto h-full p-4 text-gray-900 rounded-lg outline-0 sm:text-sm font-semibold tracking-tighter bg-white" opacity-90>
-                print("Hello, World!")
-                </p>
-              </div>
-        </div>
-      </div>
-    </div>
+    <iframe src="https://tulver.com/hb" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;">
+        Your browser doesn't support iframes
+    </iframe>
+    
   );
 };
 
